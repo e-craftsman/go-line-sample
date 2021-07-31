c8152c021dee89dda2dec2c7773df56739487b11
e-craftsman <tamotsu_watanabe@e-craftsman.com>
2021-07-31 15:16:53 +0900

    update

--- main.go
+++ main.go
@@ -1,55 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 	"net/http"
+	"os"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	// ハンドラの登録
-	http.HandleFunc("/callback", callbackHandler)
-}
-
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	// BOTを初期化
-    bot, err := linebot.New(
-        os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-        os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-    )
-    // エラーに値があればログに出力し終了する
-    if err != nil {
-        log.Fatal(err)
-    }
-
-	// リクエストからBOTのイベントを取得
-	events, err := bot.ParseRequest(r)
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
 	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
+		log.Fatal(err)
 	}
-	for _, event := range events {
-		// イベントがメッセージの受信だった場合
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			// メッセージがテキスト形式の場合
-			case *linebot.TextMessage:
-				replyMessage := message.Text
-				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-				if err != nil {
-					log.Print(err)
-				}
-			// メッセージが位置情報の場合
-			case *linebot.LocationMessage:
-//				sendRestoInfo(bot, event)
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
 			}
-			// 他にもスタンプや画像、位置情報など色々受信可能
+			return
 		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
 	}
 }
